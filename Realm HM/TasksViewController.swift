4ffd804afe7a5b3785e9fc595ace43cd3c28da21
Tatarax <dinarvaf@icloud.com>
2023-04-15 23:52:59 +0300

    Setting MainTableViewController

---
+++ Realm HM/TasksViewController.swift
@@ -0,0 +1,42 @@
+//
+//  TasksViewController.swift
+//  Realm HM
+//
+//  Created by Dinar on 15.04.2023.
+//
+
+import UIKit
+
+class TasksViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        
+
+        return cell
+    }
+    
+
+    
+
+    
+}
